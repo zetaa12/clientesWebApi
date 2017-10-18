fd612cd4a72572e5d8c5a9a786c458ef203c5bd4
cristian <hola@hola.com>
2017-10-18 03:51:30 -0300

    cambios guardados

--- ClientesWebApi/App_Start/WebApiConfig.cs
+++ ClientesWebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ClientesWebApi
 {
@@ -10,7 +11,7 @@ namespace ClientesWebApi
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
-
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             // Rutas de API web
             config.MapHttpAttributeRoutes();
 
---
+++ ClientesWebApi/Controllers/CategoriasController.cs
@@ -0,0 +1,29 @@
+﻿using ClientesWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClientesWebApi.Controllers
+{
+    public class CategoriasController : ApiController
+    {
+        private ClienteDBContext context;
+
+        public CategoriasController()
+        {
+            //instanciamos el contexto que nos permite
+            //acceso a los datos
+            context = new ClienteDBContext();
+        }
+
+        public IEnumerable<Object> get()
+        {
+            //que me retorne todas las categorias
+            return context.Categorias.ToList();
+        }
+
+    }
+}
